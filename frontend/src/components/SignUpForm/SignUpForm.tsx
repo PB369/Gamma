717d266e97304b9c2cba97b00d2b037e80802c1b
PB369 <pedrohbarros369@gmail.com>
2025-07-05 14:20:37 -0300

    SignUpForm styling is finished

--- frontend/src/components/SignUpForm/SignUpForm.tsx
+++ frontend/src/components/SignUpForm/SignUpForm.tsx
@@ -6,44 +6,46 @@ const SignUpForm = () => {
       <h2>Create your account</h2>
       <div className={styles.oAuthOptionsContainer}>
         <button className={styles.googleOAuthBtn}>
-          <img src="/frontend/public/logos/google-logo.png" alt="Google Logo" />
+          <img src="/logos/google-logo.png" alt="Google Logo" />
           <p>Google</p>
         </button>
         <button className={styles.githubOAuthBtn}>
-          <img src="/frontend/public/logos/github-logo.png" alt="GitHub Logo" />
+          <img src="/logos/github-logo.png" alt="GitHub Logo" />
           <p>GitHub</p>
         </button>
-        <div className={styles.divider}>
-          <div className={styles.divisionLine}></div> {/*Division Line*/}
-          <div className={styles.textOfDivisionLine}>Or</div>
-          <div className={styles.divisionLine}></div> {/*Division Line*/}
-        </div>
-        <form className={styles.signUpForm}>
-          <fieldset className={styles.inputsContainer}>
-            <div className={styles.firstLastNameInputsContainer}>
-              <input type="text" placeholder='Name'/>
-              <input type="text" placeholder='Last Name'/>
-            </div>
-            <input type="email" placeholder='Email'/>
-            <input type="password" placeholder='password'/>
-          </fieldset>
-          <fieldset className={styles.checkboxesContainer}>
-            <div>
-              <input type="checkbox" name="termsAndConditions"  className={styles.checkbox}/>
-              <label htmlFor="termsAndConditions">I agree with the <button>Terms and Conditions</button>.</label>
-            </div>
-            <div>
-              <input type="checkbox" name="privacyPolicy" className={styles.checkbox}/>
-              <label htmlFor="privacyPolicy">I agree with the <button>Privacy Policy</button>.</label>
-            </div>
-          </fieldset>
-          <button className={styles.signUpFormBtn}>Sign Up</button>
-          <div className={styles.signInRedirectPromptContainer}>
-            <p>Already have an account?</p>
-            <button>Sign In</button>
-          </div>
-        </form>
       </div>
+      <div className={styles.divider}>
+        <div className={styles.divisionLine}></div>
+        <div className={styles.textOfDivisionLine}>Or</div>
+        <div className={styles.divisionLine}></div>
+      </div>
+      <form className={styles.signUpForm}>
+        <fieldset className={styles.inputsContainer}>
+          <div className={styles.firstLastNameInputsContainer}>
+            <input type="text" placeholder='Name'/>
+            <input type="text" placeholder='Last Name'/>
+          </div>
+          <input type="email" placeholder='Email'/>
+          <input type="password" placeholder='Password'/>
+        </fieldset>
+        <fieldset className={styles.checkboxesContainer}>
+          <div className={styles.checkboxWrapper}>
+            <input type="checkbox" id='termsAndConditions' name='termsAndConditions' className={styles.checkbox}/>
+            <label htmlFor='termsAndConditions' className={styles.checkmark}></label>
+            <p>I agree with the <button>Terms and Conditions</button>.</p>
+          </div>
+          <div className={styles.checkboxWrapper}>
+            <input type="checkbox" id='privacyPolicy' name='privacyPolicy' className={styles.checkbox}/>
+            <label htmlFor='privacyPolicy' className={styles.checkmark}></label>
+            <p>I agree with the <button>Privacy Policy</button>.</p>
+          </div>
+        </fieldset>
+        <button className={styles.signUpFormBtn}>Sign Up</button>
+        <div className={styles.signInRedirectPromptContainer}>
+          <p>Already have an account?</p>
+          <button>Sign In</button>
+        </div>
+      </form>
     </div>
   )
 }
