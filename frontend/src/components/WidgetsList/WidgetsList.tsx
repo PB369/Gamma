6365346da739bc1fccccca2b1c66de1013f8ab29
PB369 <pedrohbarros369@gmail.com>
2025-07-10 16:17:24 -0300

    A WidgetsList animation was added

--- frontend/src/components/WidgetsList/WidgetsList.tsx
+++ frontend/src/components/WidgetsList/WidgetsList.tsx
@@ -1,17 +1,23 @@
 import { widgetsCards } from '../../utils/widgetsCardsContent'
 import WidgetCard from '../WidgetCard/WidgetCard'
 import styles from './css/WidgetsList.module.scss'
+import { motion } from 'motion/react'
 
 const WidgetsList = () => {
   return (
-    <div className={styles.widgetsListContainer}>
+    <motion.div 
+      className={styles.widgetsListContainer}
+      initial={{ x: -325, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ duration: 1, ease: 'easeInOut' }}
+    >
       <h2>List of Widgets</h2>
       <div className={styles.widgetsList}>
           {widgetsCards.map(card => (
             <WidgetCard widgetName={card.widgetName} iconName={card.iconName}/>
           ))}
       </div>
-    </div>
+    </motion.div>
   )
 }
 
