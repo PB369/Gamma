fbd12686d64e99362c24af5f8b932f0833bf8677
PB369 <pedrohbarros369@gmail.com>
2025-08-05 20:15:31 -0300

    Chat page is still under development

--- frontend/src/components/ChatsList/ChatsList.tsx
+++ frontend/src/components/ChatsList/ChatsList.tsx
@@ -1,37 +1,141 @@
+import { useState } from 'react'
+import useScreenWidth from '../../hooks/useScreenWidth'
 import ChatCard from '../ChatCard/ChatCard'
 import CreateNewChatBtn from '../CreateNewChatBtn/CreateNewChatBtn'
 import styles from './css/ChatsList.module.scss'
 import { motion } from 'motion/react'
 
 const ChatsList = () => {
+  const screenWidth = useScreenWidth();
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  const handleExpandBtn = () => {
+    setIsExpanded(prev => !prev);
+  }
+
+  const animatedFadeUp = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    transition: (delay = 0.25, duration = 0.8) => ({delay, duration})
+  }
+
   return (
-    <div className={styles.chatsListContainer}>
-      <h2>Gaia Chats</h2>
-      <CreateNewChatBtn/>
-      <div className={styles.yourChatsContainer}>
-        <p className={styles.yourChatsTitle}>Your chats</p>
-        <div className={styles.chatsList}>
-            <ChatCard/>
-            <ChatCard/>
-            <ChatCard/>
-            <ChatCard/>
-            <ChatCard/>
-            <ChatCard/>
-            <ChatCard/>
-            <ChatCard/>
-            <ChatCard/>
-            <ChatCard/>
-            <ChatCard/>
-            <ChatCard/>
-            <ChatCard/>
-            <ChatCard/>
-            <ChatCard/>
-            <ChatCard/>
-            <ChatCard/>
-            <ChatCard/>
-        </div>
-      </div>
-    </div>
+    <>
+      {
+        screenWidth < 768 ? 
+          <motion.div 
+            className={`${styles.chatsAccordionContainer} 
+            ${isExpanded && styles.expanded}`}
+            animate={{ height: isExpanded ? '70vh' : '0' }}
+            transition={{ duration: 0.8 }}
+          >
+            {
+              isExpanded && (
+                <motion.div 
+                  className={styles.mobileChatsListContainer}
+                  {...animatedFadeUp}
+                  transition={animatedFadeUp.transition()}
+                  >
+                  <div className={styles.accordionContent}>
+                    <h2>Gaia Chats</h2>
+                    <CreateNewChatBtn/>
+                    <div className={styles.yourChatsContainer}>
+                      <p className={styles.yourChatsTitle}>Your chats</p>
+                      <div className={styles.chatsList}>
+                        <ChatCard/>
+                        <ChatCard/>
+                        <ChatCard/>
+                        <ChatCard/>
+                        <ChatCard/>
+                        <ChatCard/>
+                        <ChatCard/>
+                      </div>
+                    </div>
+                  </div>
+                  {/* <h2>Gaia Chats</h2>
+                  <CreateNewChatBtn/>
+                  <div className={styles.yourChatsContainer}>
+                    <p className={styles.yourChatsTitle}>Your chats</p>
+                    <div className={styles.chatsList}>
+                        <ChatCard/>
+                        <ChatCard/>
+                        <ChatCard/>
+                        <ChatCard/>
+                        <ChatCard/>
+                        <ChatCard/>
+                        <ChatCard/>
+                    </div>
+                  </div> */}
+                </motion.div>
+              )
+            }
+            <motion.button 
+              className={`${styles.expandBtn} ${isExpanded && styles.expanded}`} 
+              onClick={handleExpandBtn}
+              animate={{ top: isExpanded ? `${window.innerHeight * 0.7 - 20}px` : '-20px' }}
+              transition={{ duration: 0.8 }}
+            >
+              <img src="/whiteIcons/arrowHeadToRight-icon.png" alt="arrowHeadToRight-icon" />
+            </motion.button>
+          </motion.div>
+          :
+          <div className={styles.desktopChatsListContainer}>
+            <h2>Gaia Chats</h2>
+            <CreateNewChatBtn/>
+            <div className={styles.yourChatsContainer}>
+              <p className={styles.yourChatsTitle}>Your chats</p>
+              <div className={styles.chatsList}>
+                  <ChatCard/>
+                  <ChatCard/>
+                  <ChatCard/>
+                  <ChatCard/>
+                  <ChatCard/>
+                  <ChatCard/>
+                  <ChatCard/>
+                  <ChatCard/>
+                  <ChatCard/>
+                  <ChatCard/>
+                  <ChatCard/>
+                  <ChatCard/>
+                  <ChatCard/>
+                  <ChatCard/>
+                  <ChatCard/>
+                  <ChatCard/>
+                  <ChatCard/>
+                  <ChatCard/>
+              </div>
+            </div>
+          </div>
+      }
+    </>
+
+    // <div className={styles.chatsListContainer}>
+    //   <h2>Gaia Chats</h2>
+    //   <CreateNewChatBtn/>
+    //   <div className={styles.yourChatsContainer}>
+    //     <p className={styles.yourChatsTitle}>Your chats</p>
+    //     <div className={styles.chatsList}>
+    //         <ChatCard/>
+    //         <ChatCard/>
+    //         <ChatCard/>
+    //         <ChatCard/>
+    //         <ChatCard/>
+    //         <ChatCard/>
+    //         <ChatCard/>
+    //         <ChatCard/>
+    //         <ChatCard/>
+    //         <ChatCard/>
+    //         <ChatCard/>
+    //         <ChatCard/>
+    //         <ChatCard/>
+    //         <ChatCard/>
+    //         <ChatCard/>
+    //         <ChatCard/>
+    //         <ChatCard/>
+    //         <ChatCard/>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 
--- frontend/src/components/CreateNewChatBtn/CreateNewChatBtn.tsx
+++ frontend/src/components/CreateNewChatBtn/CreateNewChatBtn.tsx
@@ -1,8 +1,11 @@
+import useScreenWidth from '../../hooks/useScreenWidth'
 import styles from './css/CreateNewChatBtn.module.scss'
 
 const CreateNewChatBtn = () => {
+  const screenWidth = useScreenWidth();
+
   return (
-    <button className={styles.CreateNewChatBtnContainer}>
+    <button className={`${styles.CreateNewChatBtnContainer} ${screenWidth < 768 && styles.mobileStyling}`}>
       <img className={styles.addIcon} src="/whiteIcons/add-icon.png" alt="add-icon" />
       <p>Create a new chat</p>
     </button>
