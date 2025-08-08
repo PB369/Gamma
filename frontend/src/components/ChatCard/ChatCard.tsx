7f80bc9f28476217b6501bb9db8210cef2a4b25f
PB369 <pedrohbarros369@gmail.com>
2025-08-08 19:39:58 -0300

    ChatOptions is fully finished.

--- frontend/src/components/ChatCard/ChatCard.tsx
+++ frontend/src/components/ChatCard/ChatCard.tsx
@@ -1,25 +1,76 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import styles from './css/ChatCard.module.scss'
 import ElementOptions from '../ElementOptions/ElementOptions';
+import ConfirmModal from '../ConfirmModal/ConfirmModal';
 
 const ChatCard = () => {
   const [isHovered, setIsHovered] = useState<boolean>(false);
   const [showChatOptions, setShowChatOptions] = useState<boolean>(false);
+  const [editOption, setEditOption] = useState(false);
+  const [deleteOption, setDeleteOption] = useState(false)
+  const [cursorPosition, setCursorPosition] = useState([0, 0]);
+  const [chatName, setChatName] = useState("Name of the chat Name of the chat");
+  const chatNameInputRef = useRef<HTMLInputElement>(null);
+
+  const handleDeleteChat = () => {
+    setDeleteOption(false)
+  }
+
+  const handleChatOptionsBtn = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setCursorPosition([e.clientX, e.clientY]);
+    setShowChatOptions(prev => !prev);
+  }
+
+  const handleFinishChatEdition = (e?: React.FocusEvent<HTMLInputElement> | React.KeyboardEvent<HTMLInputElement>) => {
+    if('key' in e!){
+      if(e.key !== 'Enter') return
+    }
+    setEditOption(false);
+  }
+
+  useEffect(() => {
+    if (editOption) {
+      setShowChatOptions(false)
+      chatNameInputRef.current?.focus();
+      chatNameInputRef.current?.select();
+    }
+  }, [editOption]);
 
   return (
-    <button className={styles.chatCardContainer} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-      <p>Name of the chat Name of the chat</p>
-      {isHovered && (
-        <div className={styles.chatOptionsContainer}>
-          <button onClick={() => setShowChatOptions(prev => !prev)}>
-            <div className={styles.ellipsisOfOptions}></div>
-            <div className={styles.ellipsisOfOptions}></div>
-            <div className={styles.ellipsisOfOptions}></div>
-          </button>
-          {showChatOptions && <ElementOptions elementType={'chatCard'}/>}
-        </div>
-      )}
-    </button>
+    <>
+      <button className={styles.chatCardContainer} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+        {editOption ? 
+          <input 
+            ref={chatNameInputRef} 
+            value={chatName} 
+            onChange={e=>setChatName(e.target.value)}
+            onBlur={handleFinishChatEdition}
+            onKeyDown={handleFinishChatEdition}
+          />
+          :
+          <span>{chatName}</span>
+        }
+        {isHovered && !editOption && (
+          <div className={styles.chatOptionsContainer}>
+            <button onClick={e=>handleChatOptionsBtn(e)}>
+              <div className={styles.ellipsisOfOptions}></div>
+              <div className={styles.ellipsisOfOptions}></div>
+              <div className={styles.ellipsisOfOptions}></div>
+            </button>
+          </div>
+        )}
+      </button>
+      {showChatOptions && 
+        <ElementOptions 
+          elementType={'chatCard'} 
+          setEditOption={setEditOption} 
+          setDeleteOption={setDeleteOption} 
+          cursorPosition={cursorPosition}
+          onClose={()=>setShowChatOptions(false)}
+        />
+      }
+      {deleteOption && <ConfirmModal actionType='deleteChat' onCancelAction={()=>setDeleteOption(false)} onConfirmAction={handleDeleteChat}/>}
+    </>
   )
 }
 
--- frontend/src/components/ElementOptions/ElementOptions.tsx
+++ frontend/src/components/ElementOptions/ElementOptions.tsx
@@ -1,15 +1,46 @@
+import { useEffect, useRef, type SetStateAction } from 'react';
 import styles from './css/ElementOptions.module.scss'
 
 type Props = {
   elementType: 'chatCard';
+  setEditOption: React.Dispatch<SetStateAction<boolean>>,
+  setDeleteOption: React.Dispatch<SetStateAction<boolean>>,
+  cursorPosition: number[],
+  onClose: ()=>void,
 }
 
-const ElementOptions = ({elementType}: Props) => {
+const ElementOptions = ({setEditOption, setDeleteOption, cursorPosition, onClose}: Props) => {
+  const elementOptionsRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (elementOptionsRef.current && !elementOptionsRef.current.contains(event.target as Node)) {
+        onClose();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [onClose]);
 
   return (
-    <div className={styles.elementOptionsContainer}>
-      <button>Edit</button>
-      <button>Delete</button>
+    <div 
+      className={styles.elementOptionsContainer} 
+      style={{
+        transform: `translate(${cursorPosition[0]}px, ${cursorPosition[1]}px)`,
+      }}
+      ref={elementOptionsRef}
+    >
+      <button onClick={()=>setEditOption(true)}>
+        <img src="/whiteIcons/edit-icon.png" alt="" />
+        <p>Edit</p>
+      </button>
+      <button onClick={()=>setDeleteOption(true)}>
+        <img src="/otherIcons/trash-icon.png" alt="" />
+        <p>Delete</p>
+      </button>
     </div>
   )
 }
