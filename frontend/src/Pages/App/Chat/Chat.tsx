0028aaf85470cd911ac2ef7a9270ed7f241a86fc
PB369 <pedrohbarros369@gmail.com>
2025-08-09 22:20:51 -0300

    Chat is under development

--- frontend/src/Pages/App/Chat/Chat.tsx
+++ frontend/src/Pages/App/Chat/Chat.tsx
@@ -5,6 +5,15 @@ const Chat = () => {
   return (
     <div className={styles.chatPageContainer}>
       <ChatsList/>
+      <div className={styles.chatContainer}>
+        <h2>How can I help you today, Thomas?</h2>
+        <span>
+          <input placeholder='Ask a question...'/>
+          <button>
+            <img src="/otherIcons/send-icon.png" alt="send-icon" />
+          </button>
+        </span>
+      </div>
     </div>
   )
 }
