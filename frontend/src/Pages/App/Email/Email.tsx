841cbc46553cb9f674230f5f86d59e3f2712d018
PB369 <pedrohbarros369@gmail.com>
2025-08-12 08:51:08 -0300

    Initial version of Email page has been finished

---
+++ frontend/src/Pages/App/Email/Email.tsx
@@ -0,0 +1,12 @@
+import styles from'./css/Email.module.scss'
+
+const Email = () => {
+    return (
+        <div className={styles.EmailPageContainer}>
+            <h2>Connect to your Gmail</h2>
+            <button>Let's begin</button>
+        </div>
+    )
+}
+
+export default Email
