77e9793c50b6d303a849f59cf6a67a8e5edd389b
PB369 <pedrohbarros369@gmail.com>
2025-08-28 11:43:10 -0300

    Commit

--- frontend/src/components/DateDetails/DateDetails.tsx
+++ frontend/src/components/DateDetails/DateDetails.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import type { DateDetailsType } from "../../Pages/App/CalendarPage/CalendarPage";
-
+import styles from './css/DateDetails.module.scss'
 
 type Props = {
     dateDetails: DateDetailsType | undefined;
@@ -10,44 +10,58 @@ type Props = {
 const DateDetails = ({dateDetails, setShowDateDetails}: Props) => {
     const [expandEventIndex, setExpandEventIndex] = useState<number | undefined>(undefined);
 
+    const monthsList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
     return (
-        <div>
-            <button onClick={()=>setShowDateDetails(false)}>X</button>
-            <h3>{dateDetails?.month} {dateDetails?.day}, {dateDetails?.year}</h3>
-            <p>Scheduled Activities</p>
-            <div>
+        <div className={styles.DateDetailsContainer}>
+            <button className={styles.closeBtn} onClick={()=>setShowDateDetails(false)}>X</button>
+            <h3 className={styles.dateTitle}>{dateDetails && monthsList[dateDetails?.month]} {dateDetails?.day}, {dateDetails?.year}</h3>
+            <p className={styles.evtsContainerTitle}>Scheduled Activities</p>
+            <div className={styles.eventsContainer}>
                 {dateDetails && dateDetails.events.length > 0 ?
                 dateDetails?.events.map((event, index) => {
                     const mustExpand = expandEventIndex === index 
                     return (
-                        <div onClick={()=>setExpandEventIndex(mustExpand ? undefined : index)}>
-                            <div>
-                                <p>{event.title}</p>
-                                <p>{event.time}</p>
-                            </div>
-                            <button>\/</button>
-                            {mustExpand && (
-                                <div>
-                                    <span>
-                                        <img src="" alt="" />
-                                        <p>{event.url}</p>
-                                    </span>
-                                    <span>
-                                        <img src="" alt="" />
-                                        <p>{event.address}</p>
-                                    </span>
-                                    <span>
-                                        <img src="" alt="" />
-                                        <p>{event.description}</p>
-                                    </span>
+                        <div className={styles.eventCard}>
+                            <div className={styles.leftMark}></div>
+                            <div className={styles.evtCardContent}>
+                                <div className={styles.defaultContent}>
+                                    <div onClick={()=>setExpandEventIndex(mustExpand ? undefined : index)} className={styles.titleAndTime}>
+                                        <p className={styles.title}>{event.title}</p>
+                                        <p className={styles.time}>{event.time ? `${event.time}` : "All day"}</p>
+                                    </div>
+                                    <button className={styles.checkEvtBtn}></button>
                                 </div>
-                            )}
+                                {mustExpand && (
+                                    <div className={styles.expandedDetailsContainer}>
+                                        {event.url && (
+                                            <span>
+                                                <img className={styles.infoIcon} src="/whiteIcons/location-icon.png" alt="" />
+                                                <p>{event.url}</p>
+                                            </span>
+                                        )}
+                                        {event.address && (
+                                            <span>
+                                                <img src="" alt="" />
+                                                <p>{event.address}</p>
+                                            </span>
+                                        )}
+                                        {event.description && (
+                                            <span className={styles.descriptionContainer}>
+                                                <img src="" alt="" />
+                                                <p>{event.description}</p>
+                                            </span>
+                                        )}
+                                    </div>
+                                )}
+                            </div>
                         </div>
                     )})
                     :
-                    (<p>There is no scheduled activities yet.</p>)
+                    (<p className={styles.noEvtMessage}>There is no scheduled activities yet.</p>)
                 }
             </div>
+            <button className={styles.newEvtBtn}>Schedule a new activity</button>
         </div>
     )
 }
